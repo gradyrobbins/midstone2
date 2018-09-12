d8d85c27d3d3f04bc1dcdd243d31097947928033
Grady Robbins <gradyrobbins13@gmail.com>
2018-09-12 15:09:20 -0500

    misc component experimentation

--- src/Midstone.js
+++ src/Midstone.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react"
-
+import App from './App';
 import ApplicationViews from "./ApplicationViews"
 
 class Midstone extends Component {
     render() {
         return (
-            // <React.Fragment>
-            //     <App />
-            //     <ApplicationViews />
-            // </React.Fragment>
+            <React.Fragment>
+                <App />
+                <ApplicationViews />
+            </React.Fragment>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import Midstone from './Midstone'
 
 
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Midstone />, document.getElementById('root'));

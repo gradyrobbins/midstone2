3365ca1264f600ce2916c19e612212ce061f8675
Grady Robbins <gradyrobbins13@gmail.com>
2018-10-22 08:44:19 -0500

    add template dropdown list

--- src/components/jokes/JokeList.js
+++ src/components/jokes/JokeList.js
@@ -20,6 +20,19 @@ class JokeList extends Component{
                         }>
                     Add a new coin to your collection
                 </button>
+
+                <form onSubmit="return checkForSelection();">
+                    <select id="states">
+                        <option value="AL"> Alabama</option>
+                        <option value="AK"> Alaska</option>
+                        <option value="AZ"> Arizona</option>
+                        <option value="AR"> Arkansas</option>
+                    
+                    </select>&nbsp;&nbsp;
+                </form>
+
+
+
             </div>
             <section className="jokes">
             {
